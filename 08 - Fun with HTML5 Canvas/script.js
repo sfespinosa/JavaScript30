774b4c6a8c13c31386a854dc74ee3a5e5ff2dae6
sfespinosa <scottjames.espinosa@gmail.com>
2020-12-01 11:37:49 -0800

    day 7

---
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -0,0 +1,17 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+const draw = function(e){
+    console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw)
